b55bcbd784f64f86eb1340ffea7317388770cd22
Yangyiliy-liuyang <yangyiliy@foxmail.com>
2022-05-24 14:50:56 +0800

    Changes

--- src/com/day0523/memberinnerclass/Main.java
+++ src/com/day0523/memberinnerclass/Main.java
@@ -18,6 +18,7 @@ public class Main {
         inner08Instance.say();
 
 
+
     }
 }
 
---
+++ src/com/day0524/staticinnerclass/Main.java
@@ -0,0 +1,27 @@
+package com.day0524.staticinnerclass;
+
+public class Main {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.m();
+    }
+}
+class Outer{
+    private int n=10;
+    private static String name = "张三";
+    //静态内部类
+    //1.放在外部类的成员位置
+    //2.使用static修饰
+    //3.可以使用外部类的所有静态成员，包括私有的，但是不能直接访问非静态成员
+    //4.可以添加任意访问修饰符（public protected 默认 private），因为它的地位就是一个成员。
+    public static class Inner{
+        public void say(){
+            System.out.println(name);
+        }
+    }
+
+    public void m(){
+        Inner inner = new Inner();
+        inner.say();
+    }
+}
